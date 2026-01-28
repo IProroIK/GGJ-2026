353be4b45f49d5ec9d2430b894e3c433ace5afd6
Hladkov Serhii <ibiza347@gmail.com>
2026-01-28 23:52:19 +0200

    Editor

---
+++ Assets/Editor/EditorHotkeys.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace HexGunDino.InEditor.Editor
+{
+    public class EditorHotkeys
+    {
+        // Ctrl + Shift + E (Windows) / Cmd + Shift + E (Mac)
+        [MenuItem("Hotkeys/Toggle Active %#e")]
+        private static void ToggleActive()
+        {
+            foreach (var obj in Selection.gameObjects)
+            {
+                Undo.RecordObject(obj, "Toggle Active");
+                obj.SetActive(!obj.activeSelf);
+                EditorUtility.SetDirty(obj);
+            }
+        }
+        
+        [MenuItem("Hotkeys/Toggle Active %#e", true)]
+        private static bool ToggleActiveValidate()
+        {
+            return Selection.activeGameObject != null;
+        }
+    }
+}
