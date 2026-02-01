accfd2df2d3a1757045730ddd821b8cfc9a02009
Yura Kushinskiy <grraff1230@gmail.com>
2026-02-01 07:02:12 +0200

    fix

---
+++ Assets/Scripts/TutorStep.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public sealed class TutorStep : MonoBehaviour
+    {
+        [SerializeField] private string playerTag = "Player";
+        [SerializeField] private List<GameObject> objectsToActivate;
+        [SerializeField] private TutorStep _tutorStep;
+        
+        private bool _activated;
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (_activated)
+                return;
+
+            if (!other.CompareTag(playerTag))
+                return;
+
+            Activate();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            
+            if (!other.CompareTag(playerTag))
+                return;
+
+            Deactivate();
+        }
+
+        private void Activate()
+        {
+            _activated = true;
+
+            for (int i = 0; i < objectsToActivate.Count; i++)
+            {
+                if (objectsToActivate[i] != null)
+                    objectsToActivate[i].SetActive(true);
+            }
+        }
+
+        private void Deactivate()
+        {
+            for (int i = 0; i < objectsToActivate.Count; i++)
+            {
+                if (objectsToActivate[i] != null)
+                    objectsToActivate[i].SetActive(false);
+            }
+        }
+    }
+}
